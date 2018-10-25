f6f7ca9ca74593f292a5b7c43612078ed41bd321
Justin Chen <jchen06@stuy.edu>
2018-10-25 10:14:36 -0400

    Made minor changes & updates

--- DriveSA.java
+++ DriveSA.java
@@ -3,24 +3,31 @@ public class DriveSA{
 
     SuperArray A = new SuperArray();
 
+		//Sets a new array with size 0, and should be empty
+		System.out.println("Creating new array:");
 		System.out.println("The Array: " + A);
-		System.out.println("The size of the array: " + A.size());
-		System.out.println("Is the array empty? " +  A.isEmpty());
+		System.out.println("Size of array: " + A.size());
+		System.out.println("Array empty? " +  A.isEmpty());
+		System.out.println();
+
+		//adds values to the array
 		System.out.println("Adding yuh to array: " + A.add("yuh"));
 		System.out.println("Adding getthisbread: " + A.add("getthisbread"));
+		System.out.println();
+
+		//gets indexs 1 and 0, reutrns array
 		System.out.println("Getting index 0: " + A.get(0));
 		System.out.println("Getting index 1: " + A.get(1));
-		System.out.println("Setting index 0 yur: " + A.set(0,"yur"));
-		System.out.println("Returning size: " + A.size());
-		System.out.println("The Array: " + A);
+		System.out.println("Current Array: " + A);
+		System.out.println();
 
-		for (int x = 0; x < 10; x++) {
-				A.add("t");
-		}
+		//sets index 0 to something new
+		System.out.println("Setting index 0 to: " + A.set(0,"letsgo"));
+		System.out.println("Size of array: " + A.size());
+		System.out.println("Current Array: " + A);
+		System.out.println();
 
-		System.out.println("The Array: " + A);
-		System.out.println("Adding YO: " + A.add("YO"));
-		System.out.println("The Array: " + A);
+		
   }
 }
 //for loop to make sure add(int, String)'s  int is in teh length
